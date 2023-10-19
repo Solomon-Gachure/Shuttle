1351dfbaa9589ed999537003d007fa3f2ff8c8f7
Solomon Gachure <gachuresolomon64@gmail.com>
2023-10-19 20:56:08 +0300

    Added few dependancies

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div >
+      <p className="text-orange-500 font-bold">hello</p>
+      <h2>d</h2>
+      <p>
+        h2
+      </p>
+    </div>
+  );
+}
+
+export default App;
